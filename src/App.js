e66c56e1e586d90e9c4d7a575f5e59a9e8293fa3
imfirozz <mdfiroz0630@gmail.com>
2026-04-17 00:11:17 +0530

    Add updated College Project app files

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./Components/Home";
+
+function App() {
+  return <Home />;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
